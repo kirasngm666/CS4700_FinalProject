4b3bb437e65f410b60352366976d0d442d5e66bb
Duc Nguyen <Nguyenducquocanh@gmail.com>
2023-04-16 17:25:03 -0700

    GetHit rough Animation and code
    - play rough animation when getting hit
    - getting dmg -> have an invincible period then get dmg again.

--- Breadth of the Wild Goose Project/Assets/Scripts/Player/PlayerHealth.cs
+++ Breadth of the Wild Goose Project/Assets/Scripts/Player/PlayerHealth.cs
@@ -1,30 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour, IDamageable
-{
-    public float maxHealth = 20;
-    public float health;
+// public class PlayerHealth : MonoBehaviour, IDamageable
+// {
+//     bool IsTakingDamage;
+//     bool isInvincible;
+//     bool hitSideRight;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        health = maxHealth;
-    }
+//     GooseController player = GameObject.GetComponent<GooseController>();
 
-    // Update is called once per frame
-    public virtual void ApplyDamage(float amount)
-    {
-        health -= amount;
-        if (health <= 0)
-        {
-            Die();
-        }
-    }
+//     public float maxHealth = 20;
+//     public float health;
 
-    private void Die()
-    {
-        gameObject.SetActive(false);
-    }
-}
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         health = maxHealth;
+//     }
+
+//     // Update is called once per frame
+//     public virtual void ApplyDamage(float amount)
+//     {
+//         if (!isInvincible)
+//         {
+//             currentHealth -= amount;
+//             if (currentHealth <= 0)
+//             {
+//                 Die();
+//             }
+//             else
+//             {
+//                 player.StartDamageAnimation();
+//             }
+//         }  
+//     }
+
+//     private void Die()
+//     {
+//         gameObject.SetActive(false);
+//     }
+
+//     // public void hitSide(bool rightSide)
+//     // {
+//     //     hitSideRight = rightSide;
+//     // }
+
+//     // public void Invincible(bool invincibility)
+//     // {
+//     //     isInvincible = invincibility;
+//     // }
+
+//     // void StartDamageAnimation()
+//     // {
+//     //     if (!IsTakingDamage)
+//     //     {
+//     //         IsTakingDamage = true;
+//     //         isInvincible = true;
+//     //         float hitForceX = 0.50f;
+//     //         float hitForceY = 1.5f;
+//     //         if (hitSideRight) hitForceX = -hitForceX;
+//     //         rb2d.velocity = Vector2.zero;
+//     //         rb2d.AddForce(new Vector2(hitForceX,hitForceY), ForceMode2D.Impulse);
+//     //     }
+//     // }
+
+//     // void StopDamageAnimation()
+//     // {
+//     //     IsTakingDamage = false;
+//     //     isInvincible = false;
+//     // }
+// }
