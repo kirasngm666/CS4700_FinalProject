59525fcaa06ef1bd32a018a4688070072a82572a
greatmimic <119168725+greatmimic@users.noreply.github.com>
2023-04-17 14:52:05 -0700

    Add files via upload

---
+++ Breadth of the Wild Goose Project/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string nextSceneName;
+    public GameObject transitionCanvas;
+
+    public int targetDisplay = 1; //change to this display
+
+
+    private bool transitionInProgress = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !transitionInProgress)
+        {
+            StartCoroutine(Transition());
+        }
+    }
+
+    private IEnumerator Transition()
+    {
+        transitionInProgress = true;
+
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(nextSceneName);
+        loadOperation.allowSceneActivation = false;
+
+        // Display your transition graphics here
+        transitionCanvas.SetActive(true);
+
+        while (loadOperation.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        loadOperation.allowSceneActivation = true;
+
+        // Wait for the new scene to finish loading before unloading the transition scene
+        while (!loadOperation.isDone)
+        {
+            yield return null;
+        }
+
+        SceneManager.UnloadSceneAsync("TransitionScene");
+
+        transitionInProgress = false;
+
+        //change to display 2
+        Display.displays[targetDisplay].Activate();
+    }
+}
