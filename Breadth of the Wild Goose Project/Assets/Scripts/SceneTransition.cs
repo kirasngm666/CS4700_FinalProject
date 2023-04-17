440859f7b3c00295b41155ee5347aa96ae6a9f7a
techtight <klosstyler@gmail.com>
2023-04-17 15:20:50 -0700

    scenetransition

--- Breadth of the Wild Goose Project/Assets/Scripts/SceneTransition.cs
+++ Breadth of the Wild Goose Project/Assets/Scripts/SceneTransition.cs
@@ -1,3 +1,4 @@
+//<<<<<<< Updated upstream
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
@@ -51,3 +52,60 @@ public class SceneTransition : MonoBehaviour
         Display.displays[targetDisplay].Activate();
     }
 }
+/*
+//=======
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneTransition : MonoBehaviour
+{
+    public string nextSceneName;
+    public GameObject transitionCanvas;
+
+    public int targetDisplay = 1; //change to this display
+
+
+    private bool transitionInProgress = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !transitionInProgress)
+        {
+            StartCoroutine(Transition());
+        }
+    }
+
+    private IEnumerator Transition()
+    {
+        transitionInProgress = true;
+
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(nextSceneName);
+        loadOperation.allowSceneActivation = false;
+
+        // Display your transition graphics here
+        transitionCanvas.SetActive(true);
+
+        while (loadOperation.progress < 0.9f)
+        {
+            yield return null;
+        }
+
+        loadOperation.allowSceneActivation = true;
+
+        // Wait for the new scene to finish loading before unloading the transition scene
+        while (!loadOperation.isDone)
+        {
+            yield return null;
+        }
+
+        SceneManager.UnloadSceneAsync("TransitionScene");
+
+        transitionInProgress = false;
+
+        //change to display 2
+        Display.displays[targetDisplay].Activate();
+    }
+//>>>>>>> Stashed changes
+}
+*/
