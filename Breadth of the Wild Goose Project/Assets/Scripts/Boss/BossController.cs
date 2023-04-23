82e40bd936ba34b9e5e2141bfe0fc1561b7a5616
greatmimic <tjdgns1256@gmail.com>
2023-04-23 00:30:22 -0700

    player view version update

--- Breadth of the Wild Goose Project/Assets/Scripts/Boss/BossController.cs
+++ Breadth of the Wild Goose Project/Assets/Scripts/Boss/BossController.cs
@@ -38,6 +38,85 @@ public class BossController : StateMachineBehaviour
     // Called when a transition ends and the state machine finished evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex) 
     {
+<<<<<<< Updated upstream
         animator.ResetTrigger("Attack");
+=======
+        if (Time.time - lastAttackTime > attackCooldown)
+        {
+            //animator.SetBool("isRunning", false);
+            //animator.SetBool("isAttacking", true);
+            //player.GetComponent<GooseController>().hitSide(transform.position.x > player.transform);
+            gooseController.hitSide(transform.position.x > player.transform.position.x);
+            //player.GetComponent<GooseController>().ApplyDamage(attackDamage);
+            gooseController.ApplyDamage(attackDamage);
+            lastAttackTime = Time.time;
+            Debug.Log("GOOSE THE BOSS is pecking");
+        }
+        else
+        {
+            //animator.SetBool("isRunning", true);
+            //animator.SetBool("isAttacking", false);
+        }
+    }
+
+    public virtual void ApplyDamage(float amount)
+    {
+        if (!isInvincible)
+        {
+            hitCount++;
+            currentHealth -= amount;
+            BossUIHealthBar.instance.SetValue(currentHealth / (float)healthPool);
+            if (currentHealth <= 0)
+            {
+                Die();
+            }
+            else
+            {
+               if (hitCount == 3)
+               {
+                    StartEnemyDamageAnimation(); 
+               } 
+            }
+        }  
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+        GameManagerController.instance.BossDefeated();
+    }
+
+    public void hitSide(bool rightSide)
+    {
+        hitSideRight = rightSide;
+    }
+
+    public void Invincible(bool invincibility)
+    {
+        isInvincible = invincibility;
+    }
+
+    public void StartEnemyDamageAnimation()
+    {
+        if (!IsTakingDamage)
+        {
+            IsTakingDamage = true;
+            isInvincible = true;
+            hitCount = 0;
+            float hitForceX = 500f;
+            float hitForceY = 500f;
+            if (hitSideRight) hitForceX = -hitForceX;
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(new Vector2(hitForceX,hitForceY), ForceMode.Impulse);
+        }
+    }
+
+    void StopEnemyDamageAnimation()
+    {
+        IsTakingDamage = false;
+        isInvincible = false;
+        animator.Play("Boss_Idle", -1, 0f);
+
+>>>>>>> Stashed changes
     }
 }
