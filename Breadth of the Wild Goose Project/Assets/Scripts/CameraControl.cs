a6d4b764552d5b4cf8d693aa1636f704a305884d
greatmimic <tjdgns1256@gmail.com>
2023-04-22 22:46:56 -0700

    camera control edits

--- Breadth of the Wild Goose Project/Assets/Scripts/CameraControl.cs
+++ Breadth of the Wild Goose Project/Assets/Scripts/CameraControl.cs
@@ -1,19 +1,16 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-
-
 
 public class CameraControl : MonoBehaviour
 {
     public Transform player;
     public Transform nextSection;
+    public float smoothTime = 0.3f;
 
     private GameManagerController gameManager;
     private bool isPlayerTeleported = false;
-
+    private Vector3 velocity = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -24,20 +21,17 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
         //if 4 enemies have been beaten, move camera to boss section
-       if (GameManagerController.instance != null && GameManagerController.instance.enemiesBeaten >= 4)
+        if (GameManagerController.instance != null && GameManagerController.instance.enemiesBeaten >= 4)
         {
-            
-
             if (!isPlayerTeleported)
             {
-                Vector2 newPosition = new Vector2(nextSection.position.x - 3f, nextSection.position.y);
+                Vector3 newPosition = new Vector3(nextSection.position.x - 3f, nextSection.position.y, nextSection.position.z);
                 player.position = newPosition;
                 isPlayerTeleported = true;
             }
-            transform.position = new Vector2(player.position.x, player.position.y);
 
+            transform.position = Vector3.SmoothDamp(transform.position, nextSection.position, ref velocity, smoothTime);
         }
         //if no boss fight, camera follows player
         else
